416d607142a8e1c0f3f9e2dd7e76d038a979c3f6
Prince Prakalp <30505797+algorithm98@users.noreply.github.com>
2024-03-28 19:29:23 +0530

    role and credential rerrors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,16 +21,16 @@ export default async function Home() {
       <h1 className="bg-purple-300 w-80 h-12 justify-center font-bold p-4 rounded-md mb-6">Name : {session.user?.name}</h1>
       <br />
       <h1 className="bg-purple-300 w-80 h-12 justify-center font-bold p-4 rounded-md mb-6">Email : {session.user?.email}</h1>
-      <br />
-      <h1 className="bg-purple-300 w-80 h-12 justify-center font-bold p-4 rounded-md mb-6">Type :  {session.user?.role}</h1>
+      {/* <br />
+      <h1 className="bg-purple-300 w-80 h-12 justify-center font-bold p-4 rounded-md mb-6">Type :  {session.user?.role}</h1> */}
       <br />
       {/* <h1 className="bg-purple-300 w-80 h-12 justify-center font-bold p-4 rounded-md mb-6">Contact : </h1>
       <br /> */}
       <span className="font-bold text-3xl">Credentials</span>
       <br />
-      <div className="bg-purple-300 border border-500 w-80 h-12 rounded-lg">
+      {/* <div className="bg-purple-300 border border-500 w-80 h-12 rounded-lg">
       <h1 className="bg-purple-300 w-80 h-12 justify-center font-bold p-4 rounded-md mb-6">{session.user?.Credentials}</h1>
-      </div>
+      </div> */}
     </>
 
     {/* <p>{JSON.stringify(session)}</p> */}
